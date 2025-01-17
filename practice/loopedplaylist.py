860ae1a7cf2819e5b10564bb495dbd80ffa6ae8a
krishnakanth <krishnakanthpathi@gmail.com>
2025-01-17 12:41:35 +0530

    new files are added

--- practice/loopedplaylist.py
+++ practice/loopedplaylist.py
@@ -10,10 +10,25 @@ from functools import cmp_to_key, lru_cache
 from fractions import Fraction
 
 def main() :
-    n = int(input())
-    a,b = list(map(int , input().split()))
-    
-    
-
-t = int(input())
-for i in range(t) : main()
+    n, k = list(map(int , input().split()))
+    arr = list(map(int, input().split()))
+    total = sum(arr)
+    minsongs = float("inf")
+    songs = k//total
+    if k%total == 0 : 
+        print(*[1 , songs*n])
+        return
+    rem = k%total 
+    for i in range(n):
+        cur = 0
+        cnt = 0
+        for j in range(i , i + n):
+            if cur >= rem :
+                break
+            cnt += 1
+            cur += arr[j%n]
+        if cnt < minsongs :
+            minsongs = cnt
+            start = i + 1
+    print(start ,(songs)*n + minsongs)
+main()
