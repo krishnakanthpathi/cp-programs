d75f337925ea734ff508982dc102aeebe850aa9a
krishnakanth <krishnakanthpathi@gmail.com>
2025-02-27 22:20:15 +0530

    new things

--- practice/readingbooks.py
+++ practice/readingbooks.py
@@ -1,11 +1,14 @@
-n = int(input())
-books = list(map(int, input().split()))
+def solve():
+    n = int(input())
+    a = list(map(int, input().split()))
+    
+    max_value = -1
+    total_sum = 0
+    
+    for x in a:
+        total_sum += x
+        max_value = max(max_value, x)
+    
+    print(max(max_value * 2, total_sum))
 
-import heapq
-
-heapq.heapify(books)
-
-frist = second = 0
-
-
-
+solve()
