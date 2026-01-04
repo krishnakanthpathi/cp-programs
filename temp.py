36e9f51c108e13d62b7767643004dde9a3ec7441
krishnakanthpathi <krishnakanthpathi@gmail.com>
2026-01-04 10:11:39 +0530

    added changes

--- temp.py
+++ temp.py
@@ -1,17 +1,44 @@
+from typing import List
+from collections import defaultdict
 
 
-n = int(input())
-arr = list(map(int, input().split()))
-k = int(input())
+class Solution:
+    def minLength(self, nums: List[int], k: int) -> int:
+        l = 1
+        n = len(nums)
+        r = n
+        res = float("inf")
+        while l <= r :
+            mid = (l + r) // 2
+            flag = False
+            cur = 0
+            left = 0
+            hashset = defaultdict(int)
+            for right in range(n) :
+                if right - left + 1 > mid :
+                    hashset[nums[left]] -= 1
+                    if hashset[nums[left]] <= 0 :
+                        cur -= nums[left]
+                        hashset.pop(nums[left])
+                    left += 1
+                if nums[right] not in hashset :
+                    cur += nums[right]
+                if cur >= k :
+                    flag = True
+                    break
+                hashset[nums[right]] += 1
+            if flag :
+                res = mid
+                r = mid - 1
+            else:
+                l = mid + 1
+        return res if res != float("inf") else -1
 
-minimus = []
-slist = []
-for i in range(n):
-    slist.append(arr[i])
-    slist.sort()
-    if i >= k:
-        slist.remove(arr[i - k])
-    if i >= k - 1:
-        minimus.append(slist[0])
-
-print(max(minimus))
+ans = Solution()
+# nums =
+# [22,8,27,13,12,26,2]
+# k =
+# 51©leetcode
+nums = [22,8,27,13,12,26,2]
+k = 51
+print(ans.minLength(nums, k))
