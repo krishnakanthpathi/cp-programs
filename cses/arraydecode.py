4c5219d8d1b377149e471f3abfe43f3be0ddc931
krishnakanthpathi <krishnakanthpathi@gmail.com>
2026-07-02 14:16:49 +0000

    refactor: update arraydecode solution to use iterative DP

--- cses/arraydecode.py
+++ cses/arraydecode.py
@@ -9,26 +9,64 @@ arr = list(map(int , input().split()))
 
 MOD = int(1e9 + 7)
 
-def solve(n , k , arr) :
-    @cache
-    def dfs(idx , prev) :
 
-        if idx == n :
-            return 1
+dp = [
+  [ 0  for j in range(m + 1) ] for i in range(n + 1)
+]
+
+
+
+for i in range(n) :
+    for j in range(1 , m + 1):
+        if arr[i] != 0 :
+            dp[i][j] += 1 + dp[i - 1][j] if i - 1 >= 0 else 1
+            continue
         
-        if arr[idx] != 0 :
-            if abs(arr[idx] - prev) > 1 :
-                return 0
-            return dfs(idx + 1 , arr[idx])
+        if i == 0 :
+            # yet to be implemented
+            continue
 
-        res = 0
-        for i in range(1 , m + 1) :
-            if abs(i - prev) <= 1 or prev == float("inf"):
-                ans = dfs(idx + 1 , i)
-                res =  (res +  ans )% MOD 
-        return res 
+        if abs(arr[i - 1] - j) > 1  :
+            continue
 
-
-    return dfs(0 , float("inf"))
+        dp[i][j] += dp[i - 1][j] + 1
+    print(dp[i] )
+print(dp[n][m])
         
-print(solve(n , m , arr))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def solve(n , k , arr) :
+#     @cache
+#     def dfs(idx , prev) :
+
+#         if idx == n :
+#             return 1
+        
+#         if arr[idx] != 0 :
+#             if abs(arr[idx] - prev) > 1 :
+#                 return 0
+#             return dfs(idx + 1 , arr[idx])
+
+#         res = 0
+#         for i in range(1 , m + 1) :
+#             if abs(i - prev) <= 1 or prev == float("inf"):
+#                 ans = dfs(idx + 1 , i)
+#                 res =  (res +  ans )% MOD 
+#         return res 
+
+
+#     return dfs(0 , float("inf"))
+        
+# print(solve(n , m , arr))
